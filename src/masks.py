ec750d1fc2b1bd8271f3c536a1d908baf8492fc7
Artemova Dina <rayour@gmail.com>
2025-05-04 17:48:50 +0300

    initial commit

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: int | str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+
+    string_card_number = str(card_number)
+    return f"{string_card_number[0:4]} {string_card_number[4:6]}** **** {string_card_number[-4:]}"
+
+
+def get_mask_account(account: int | str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+
+    string_account = str(account)
+    return f"**{string_account[-4:]}"
