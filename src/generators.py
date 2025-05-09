273038baf9f00b9a2e7b6dac267d524444765eb0
Artemova Dina <rayour@gmail.com>
2025-05-09 09:49:56 +0300

    update tests for filter_by_currency function

--- src/generators.py
+++ src/generators.py
@@ -1,1 +1,15 @@
 """Модуль содержит функции, реализующие генераторы для обработки данных"""
+
+from typing import Generator, Iterator
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator | Generator:
+    """Принимает на вход список словарей, представляющих транзакции.
+    Возвращать итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной (например, USD)."""
+
+    sequence = (
+        transaction for transaction in transactions if transaction["operationAmount"]["currency"]["code"] == currency
+    )
+
+    return sequence
---
+++ tests/test_generators.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+import pytest
+
+import src.generators
+
+
+@pytest.mark.parametrize("correct_transactions_list", [i for i in range(2)], indirect=True)
+def test_filter_by_currency(correct_transactions_list: Any) -> None:
+    gen = src.generators.filter_by_currency(*correct_transactions_list["input"])
+    for i in range(len(correct_transactions_list["output"])):
+        assert next(gen) == correct_transactions_list["output"][i]
+
+
+@pytest.mark.parametrize("empty_transactions_list", [i for i in range(2)], indirect=True)
+def test_filter_by_currency_empty(empty_transactions_list: Any) -> None:
+
+    gen = src.generators.filter_by_currency(*empty_transactions_list["input"])
+    with pytest.raises(StopIteration) as exc_info:
+        next(gen)
+
+    assert str(exc_info.typename) == empty_transactions_list["output"]
