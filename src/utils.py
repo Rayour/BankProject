16f14bf57963ad30d8c45e77795550b989dd263c
Artemova Dina <rayour@gmail.com>
2025-06-08 10:47:21 +0300

    add util modul with get operations list function

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def get_operations_list(path_to_json: str) -> list[dict]:
+    """Получает на вход путь до json файла, возвращает список словарей с информацией об операциях"""
+
+    PATH_TO_JSON = os.path.join('..', path_to_json)
+
+    try:
+        with open(PATH_TO_JSON, 'r', encoding='utf-8') as json_file:
+            operations_list = json.load(json_file)
+            if isinstance(operations_list, list) and operations_list:
+                return operations_list
+            else:
+                return []
+    except FileNotFoundError:
+        print("En error occurred. File not found. Please, check the file or path and try again.")
+        return []
