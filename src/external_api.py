90d6dbd83be879b35af9bd45fcb0693ec9bc4b37
Artemova Dina <rayour@gmail.com>
2025-06-09 08:21:04 +0300

    remove transaction date from exchange

--- src/external_api.py
+++ src/external_api.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def exchange_money(amount_currency: float, currency_from_convert: str, transaction_date: str) -> float | Any:
-    """Получает на вход сумму в рублях, валюту для конвертации и дату в формате YYYY-mm-dd,
+def exchange_money(amount_currency: float, currency_from_convert: str) -> float | Any:
+    """Получает на вход сумму в рублях и валюту для конвертации,
     возвращает сумму в указанной валюте"""
 
     exchange_service_api_key = os.getenv("EXCHANGE_SERVICE_API_KEY")
     headers = {"apikey": exchange_service_api_key}
-    params = {"to": "RUB", "from": currency_from_convert, "amount": str(amount_currency), "date": transaction_date}
+    params = {"to": "RUB", "from": currency_from_convert, "amount": str(amount_currency)}
 
     response = requests.get("https://api.apilayer.com/exchangerates_data/convert", headers=headers, params=params)
 
