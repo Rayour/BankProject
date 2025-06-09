727e361adad6c4e585f09be43722c2726b7b7c9d
Artemova Dina <rayour@gmail.com>
2025-06-09 08:51:32 +0300

    add get_transactions_sum function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import json
 import os
 from pathlib import Path
 
+from src.external_api import exchange_money
+
 
 def get_operations_list(path_to_json: str) -> list[dict]:
     """Получает на вход путь до json файла, возвращает список словарей с информацией об операциях"""
@@ -18,3 +20,22 @@ def get_operations_list(path_to_json: str) -> list[dict]:
     except FileNotFoundError:
         print("En error occurred. File not found. Please, check the file or path and try again.")
         return []
+
+
+def get_transactions_sum(transactions: list[dict]) -> float:
+    """Получает на вход список словарей с транзакциями, возвращает сумму всех транзакций в рублях.
+    Конвертация из других валют происходит по текущему курсу на момент расчетов"""
+
+    transactions_sum = 0.0
+
+    for transaction in transactions:
+        print(transaction)
+        if transaction["operationAmount"]["currency"]["code"] == "RUB":
+            transactions_sum += float(transaction["operationAmount"]["amount"])
+        else:
+            amount_rub = exchange_money(
+                transaction["operationAmount"]["amount"], transaction["operationAmount"]["currency"]["code"]
+            )
+            transactions_sum += float(amount_rub)
+
+    return transactions_sum
