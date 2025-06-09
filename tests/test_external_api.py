0ad8ec14cb3c426b7c50fa328d2e0c6a756a92c4
Artemova Dina <rayour@gmail.com>
2025-06-09 08:22:03 +0300

    remove transaction date from exchange

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -11,7 +11,7 @@ import src.external_api
 def test_exchange_money(mocked_get: Any, correct_exchange_money_data_for_mock: Any) -> None:
     mocked_get.return_value.json.return_value = correct_exchange_money_data_for_mock["input"]["json"]
     mocked_get.return_value.status_code = correct_exchange_money_data_for_mock["input"]["status"]
-    assert src.external_api.exchange_money(5, "USD", "2025-03-03") == \
+    assert src.external_api.exchange_money(5, "USD") == \
            correct_exchange_money_data_for_mock["output"]
 
 
@@ -22,6 +22,6 @@ def test_exchange_money_exception(mocked_get: Any, incorrect_exchange_money_data
     mocked_get.return_value.status_code = incorrect_exchange_money_data_for_mock["input"]["status"]
 
     with pytest.raises(Exception) as exc_info:
-        src.external_api.exchange_money(5, "USD", "2025-03-03")
+        src.external_api.exchange_money(5, "USD")
 
     assert str(exc_info.value) == incorrect_exchange_money_data_for_mock["output"]
