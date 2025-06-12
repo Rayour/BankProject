3a2462f9954ccf097b420a58e3b3810b75ab34a1
Artemova Dina <rayour@gmail.com>
2025-06-12 15:42:18 +0300

    naming fix

--- src/read_data.py
+++ src/read_data.py
@@ -44,7 +44,7 @@ def read_transactions_from_csv(csv_path: str) -> list[dict]:
         return transactions_list
 
 
-def get_transactions_from_xlsx(xlsx_path: str) -> list[dict] | Any:
+def read_transactions_from_xlsx(xlsx_path: str) -> list[dict] | Any:
     """Функция получает на вход путь до xlsx файла с транзакциями,
     возвращает список транзакций"""
 
--- tests/test_logs/test_read_data.py
+++ tests/test_logs/test_read_data.py
@@ -10,23 +10,23 @@ import src.read_data
 
 @pytest.mark.parametrize("transactions_list_for_read_from_csv", [i for i in range(2)], indirect=True)
 @patch("csv.DictReader")
-def test_get_transactions_from_csv(mocked_file: Any, transactions_list_for_read_from_csv: Any) -> None:
+def test_read_transactions_from_csv(mocked_file: Any, transactions_list_for_read_from_csv: Any) -> None:
     mocked_file.return_value = transactions_list_for_read_from_csv["input"]
     assert src.read_data.read_transactions_from_csv(os.path.join('data', 'transactions.csv')) == \
            transactions_list_for_read_from_csv["output"]
 
 
-def test_get_transactions_from_csv_exception() -> None:
+def test_read_transactions_from_csv_exception() -> None:
     assert src.read_data.read_transactions_from_csv('fake_transactions.csv') == []
 
 
 @pytest.mark.parametrize("transactions_from_xlsx", [i for i in range(2)], indirect=True)
 @patch("pandas.read_excel")
-def test_get_transactions_from_xlsx(mocked_df: Any, transactions_from_xlsx: Any) -> None:
+def test_read_transactions_from_xlsx(mocked_df: Any, transactions_from_xlsx: Any) -> None:
     mocked_df.return_value = pd.DataFrame(transactions_from_xlsx["input"])
-    assert src.read_data.get_transactions_from_xlsx(os.path.join('data', 'transactions_excel.xlsx')) == \
+    assert src.read_data.read_transactions_from_xlsx(os.path.join('data', 'transactions_excel.xlsx')) == \
            transactions_from_xlsx["output"]
 
 
-def test_get_transactions_from_xlsx_exception() -> None:
-    assert src.read_data.get_transactions_from_xlsx('fake_transactions_excel.xlsx') == []
+def test_read_transactions_from_xlsx_exception() -> None:
+    assert src.read_data.read_transactions_from_xlsx('fake_transactions_excel.xlsx') == []
