5c993d7895d8780a83e53a1d37520e7d42e018e1
Artemova Dina <rayour@gmail.com>
2025-06-12 15:08:06 +0300

    add function to read transactions from xlsx

--- src/read_data.py
+++ src/read_data.py
@@ -1,6 +1,10 @@
 import csv
+import json
 import os.path
 from pathlib import Path
+from typing import Any
+
+import pandas as pd
 
 ROOT_PATH = Path(__file__).resolve().parents[1]
 
@@ -16,7 +20,9 @@ def read_transactions_from_csv(csv_path: str) -> list[dict]:
         with open(full_csv_path, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file, delimiter=';')
             for row in reader:
-                transactions_list.append(row)
+                if row['id']:
+                    row['id'] = int(row['id'])
+                    transactions_list.append(row)
     except Exception as ex:
         print(f"Error: {ex}")
         return []
@@ -24,4 +30,20 @@ def read_transactions_from_csv(csv_path: str) -> list[dict]:
         return transactions_list
 
 
-print(read_transactions_from_csv(os.path.join('transactions.csv')))
+def get_transactions_from_xlsx(xlsx_path: str) -> list[dict] | Any:
+    """Функция получает на вход путь до xlsx файла с транзакциями,
+    возвращает список транзакций"""
+
+    full_xlsx_path = os.path.join(ROOT_PATH, xlsx_path)
+
+    try:
+        transactions_df = pd.read_excel(full_xlsx_path)
+        not_empty_transactions_df = transactions_df.loc[transactions_df.id.notnull()]
+        not_empty_transactions_df.id = not_empty_transactions_df.id.map(int)
+        not_empty_transactions_df.amount = not_empty_transactions_df.amount.map(str)
+        transactions_list = json.loads(not_empty_transactions_df.to_json(orient='records'))
+    except Exception as ex:
+        print(f"Error: {ex}")
+        return []
+    else:
+        return transactions_list
--- tests/test_logs/test_read_data.py
+++ tests/test_logs/test_read_data.py
@@ -2,6 +2,7 @@ import os.path
 from typing import Any
 from unittest.mock import patch
 
+import pandas as pd
 import pytest
 
 import src.read_data
@@ -17,3 +18,15 @@ def test_get_transactions_from_csv(mocked_file: Any, transactions_list_for_read_
 
 def test_get_transactions_from_csv_exception() -> None:
     assert src.read_data.read_transactions_from_csv('fake_transactions.csv') == []
+
+
+@pytest.mark.parametrize("transactions_from_xlsx", [i for i in range(2)], indirect=True)
+@patch("pandas.read_excel")
+def test_get_transactions_from_xlsx(mocked_df: Any, transactions_from_xlsx: Any) -> None:
+    mocked_df.return_value = pd.DataFrame(transactions_from_xlsx["input"])
+    assert src.read_data.get_transactions_from_xlsx(os.path.join('data', 'transactions_excel.xlsx')) == \
+           transactions_from_xlsx["output"]
+
+
+def test_get_transactions_from_xlsx_exception() -> None:
+    assert src.read_data.get_transactions_from_xlsx('fake_transactions_excel.xlsx') == []
