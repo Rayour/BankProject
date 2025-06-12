2973576cd84afdd69feb81501a05d2d95ebdefa4
Artemova Dina <rayour@gmail.com>
2025-06-12 15:37:59 +0300

    reformat transactions output

--- src/read_data.py
+++ src/read_data.py
@@ -21,8 +21,22 @@ def read_transactions_from_csv(csv_path: str) -> list[dict]:
             reader = csv.DictReader(file, delimiter=';')
             for row in reader:
                 if row['id']:
-                    row['id'] = int(row['id'])
-                    transactions_list.append(row)
+                    new_row = {
+                        'id': int(row['id']),
+                        'state': row['state'],
+                        'date': row['date'],
+                        'description': row['description'],
+                        'operationAmount': {
+                            "amount": row['amount'],
+                            "currency": {
+                                "name": row['currency_name'],
+                                "code": row['currency_code']
+                            }
+                        },
+                        'to': row['to'],
+                        'from': row['from']
+                    }
+                    transactions_list.append(new_row)
     except Exception as ex:
         print(f"Error: {ex}")
         return []
@@ -39,11 +53,27 @@ def get_transactions_from_xlsx(xlsx_path: str) -> list[dict] | Any:
     try:
         transactions_df = pd.read_excel(full_xlsx_path)
         not_empty_transactions_df = transactions_df.loc[transactions_df.id.notnull()]
-        not_empty_transactions_df.id = not_empty_transactions_df.id.map(int)
-        not_empty_transactions_df.amount = not_empty_transactions_df.amount.map(str)
         transactions_list = json.loads(not_empty_transactions_df.to_json(orient='records'))
+        formated_transactions_list = []
+        for transaction in transactions_list:
+            formated_transaction = {
+                                        'id': int(transaction['id']),
+                                        'state': transaction['state'],
+                                        'date': transaction['date'],
+                                        'description': transaction['description'],
+                                        'operationAmount': {
+                                            "amount": str(transaction['amount']),
+                                            "currency": {
+                                                "name": transaction['currency_name'],
+                                                "code": transaction['currency_code']
+                                            }
+                                        },
+                                        'to': transaction['to'],
+                                        'from': transaction['from']
+                                    }
+            formated_transactions_list.append(formated_transaction)
     except Exception as ex:
         print(f"Error: {ex}")
         return []
     else:
-        return transactions_list
+        return formated_transactions_list
